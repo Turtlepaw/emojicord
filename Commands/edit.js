ef0fcb0c097fad887104f6fc6012c21aafa08e29
TurtlePaw <turtlesimsyt@gmail.com>
2022-02-04 16:18:12 -0500

    Stuff

--- Commands/edit.js
+++ Commands/edit.js
@@ -4,7 +4,8 @@ const Embed = require("../Util/Embed");
 const { Emojis } = require("../Util/EmojiManager");
 const DiscordReal = require("discord.js");
 const Jimp = require("jimp");
-const { errorMessage, checkPermissions } = require("../Util/util");
+const { errorMessage, checkPermissions, fixText } = require("../Util/util");
+const { generateDefualtFilter, FilterTypes } = require("../Config/config");
 
 module.exports = {
     data: new jsh.commandBuilder()
@@ -41,11 +42,14 @@ module.exports = {
         //Make sure it does not execute the command
         if(!Perms) return;
 
+        //Get emojis
         const RawEmoji = interaction.options.getString("emoji");
         const Emoji = interaction.guild.emojis.cache.find(e => e.name == RawEmoji);
 
+        //Check to make sure they used the autocomplete
         if(!Emoji) return errorMessage(`Emoji not found`, interaction);
 
+        //Create embeds and componenets
         const BaseOptions = new DiscordReal.MessageSelectMenu()
         .setCustomId("edit_options")
         .setMaxValues(1)
@@ -91,29 +95,91 @@ module.exports = {
             .addComponents(DisOptions)
         ];
 
-        const BaseEmbed = new Embed()
-        .setTitle(`${Emojis.update_dc.show} | Editing an emoji`)
-        .setDescription(`Your editing the emoji \`${Emoji.name}\`!`)
-        .setThumbnail(Emoji.url);
+        const EmojiFilters = {
+            "ROLES": "ROLES",
+            "ROUND": "ROUND",
+            "RECOLOR": "RECOLOR",
+            "NAME": "NAME"
+        };
+        const EmojiFilterDef = "ROLES" || "ROUND" || "RECOLOR" || "NAME";
+
+        /**
+         * Builds the default embed.
+         * @param {EmojiFilterDef[]} filters The filters applied to the emoji
+         * @returns {Embed}
+         */
+        function buildDefualtEmbed(filters=[]){
+            const MappedFilters = [];
+            let i;
+            for(const filter of filters){
+                MappedFilters.push(`${i == filters.length ? Emojis.reply_1.show : Emojis.stem_1.show} ${fixText(filter)}`);
+            };
+
+            return new Embed()
+            .setTitle(`${Emojis.update_dc.show} | Editing an emoji`)
+            .setDescription(`Your editing the emoji \`${Emoji.name}\`!`)
+            .setThumbnail(Emoji.url)
+            .addField(`${Emojis.search_dc.show} Name`, `\`${Emoji.name}\``)
+            .addField(`${Emojis.dev_dc.show} ID & Emoji`, `${Emojis.stem_1.show} \`${Emoji}\`\n${Emojis.reply_1.show} \`${Emoji.id}\``)
+            .addField(`${Emojis.star_dc.show} Filters`, `${filters.length >= 1 ? MappedFilters.join(`\n`) : "\`No filters applied\`"}`)
+        }
         
         const EndEmbed = new Embed()
         .setTitle(`${Emojis.delete_dc.show} | Edit emoji menu closed`);
 
+        //Send the message
         /**
          * @type {DiscordReal.Message}
          */
         const Message = await interaction.reply({
-            embeds: BaseEmbed.build(),
+            embeds: buildDefualtEmbed().build(),
             components: Rows,
             fetchReply: true
         });
 
+        //Create a `collect()` function to make it easier
+        /**
+         * An easy shortcut to `channel.awaitMessages`
+         * @param {DiscordReal.TextChannel} TextChannel The channel to await messages
+         * @param {Function} cb The callback
+         * @returns {Promise<DiscordReal.Message> | "CANCEL"}
+         */
+        async function collect(TextChannel, cb=null){
+            const message = await TextChannel.awaitMessages({
+                filter: generateDefualtFilter(FilterTypes.MESSAGE, interaction.user.id),
+                max: 1
+            });
+
+            if(cb != null) await cb(message);
+            if(message.first().content.toLowerCase().startsWith("cancel")) return "CANCEL";
+            return message.first();
+        }
+
+        //Create the collector
         const collector = Message.createMessageComponentCollector({
-            filter: i=>i.user.id==interaction.user.id
+            filter: generateDefualtFilter(FilterTypes.INTERACTION, interaction.user.id)
         });
 
+        const Filters = [];
+
         collector.on("collect", async i => {
-            if(!i.isSelectMenu()) return;
+            async function cancel(){
+                return await update();
+            }
+
+            async function update(Embed=BaseEmbed, componenets=Rows, forceNoBuild=false, fetchReply=true){
+                if(i.replied) return interaction.editReply({
+                    embeds: forceNoBuild == true ? Embed : Embed.build(),
+                    components: componenets,
+                    fetchReply
+                });
+
+                return await i.update({
+                    embeds: forceNoBuild == true ? Embed : Embed.build(),
+                    components: componenets,
+                    fetchReply
+                });
+            }
             const val = i.values[0];
 
             if(val == "EXIT"){
@@ -121,6 +187,17 @@ module.exports = {
                     embeds: EndEmbed.build(),
                     components: RowsDisabled
                 });
+            } if(val == "NAME"){
+                await update(new Embed().setTitle(`${Emojis.clock_dc.show} | Awaiting text`).setDescription(`Send the new Emoji name!`))
+                const Message = await collect(i.channel);
+
+                if(Message == "CANCEL") return await cancel();
+
+                await Emoji.setName(Message.content, `This action has been done by ${i.user.tag} (${i.user.id})`);
+
+                Filters.push(EmojiFilters.NAME);
+
+                await update(buildDefualtEmbed(Filters));
             }
         });
     }
--- Config/config.js
+++ Config/config.js
@@ -1,10 +1,12 @@
 const { Client } = require("discord.js");
 const Discord = require("discord.js");
 const { Colors } = require("discordjsh");
+const { clientID } = require("./config.json");
 
 const WebsiteURL = module.exports.WebsiteURL = `https://emojis.trtle.xyz/`;
+const BetaBotID = "938640166820675585";
 module.exports.InviteURL = WebsiteURL != null ? WebsiteURL + "invite" : ``;
-module.exports.Color = Colors.BLURPLE;
+module.exports.Color = clientID == BetaBotID ? "#D0473A" : Colors.BLURPLE;
 module.exports.SupportURL = WebsiteURL != null ? WebsiteURL + "support" : ``;
 module.exports.TestGuildID = `842575277249921074`;
 /**
@@ -25,6 +27,24 @@ module.exports.GenerateInviteURL = (client) => {
         scopes: this.scopes
     });
 };
+const FilterTypes = module.exports.FilterTypes = {
+    "INTERACTION": "INTERACTION",
+    "MESSAGE": "MESSAGE"
+};
+const FilterDef = module.exports.FilterDef = "INTERACTION" || "MESSAGE";
+/**
+ * Generates a filter
+ * @param {FilterDef} type The type of filter
+ * @param {String} matchId The userId to match
+ * @returns {Discord.InteractionCollector | Discord.MessageCollector} The collector filter
+ */
+module.exports.generateDefualtFilter = (type, matchId) => {
+    if(type == "INTERACTION"){
+        return i => i.user.id == matchId;
+    } else if(type == "MESSAGE"){
+        return m => m.author.id == matchId;
+    }
+}
 module.exports.URL = {
     Invite: WebsiteURL + "invite"
 };
